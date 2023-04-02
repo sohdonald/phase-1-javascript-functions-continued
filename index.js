f1431d5453407936bf0dd1c5211d8b7d0d53c807
sohdonald <sohdonald@myUbuntu.myguest.virtualbox.org>
2023-04-02 07:46:57 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+//3 down
+
+function mondayWork(work = 'go to the office') {
+    return `This Monday, I will ${work}.`;
+}
+
+// 6 down
+
+function wrapAdjective(wrapper = '*') {
+    return function (adj = 'special') {
+        return `You are ${wrapper}${adj}${wrapper}!`;
+    } 
+}
+
+const encouragingPromptFunction = wrapAdjective("!!!");
